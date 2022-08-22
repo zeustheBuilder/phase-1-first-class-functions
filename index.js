3f6192d83600289ed6ce7ef7ebacc3cf95ac3ed6
Brian <bgitau35@gmail.com>
2022-08-22 12:24:21 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(students) {
+    students();   }      
+
+function returnsANamedFunction() { 
+   return function namedFn() 
+   { 
+   console.log("this is a named function"); 
+};  
+
+}      
+
+function returnsAnAnonymousFunction() { 
+return () => console.log("this is an anonymous function");  
+} 
